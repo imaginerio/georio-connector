8a140ae3a4834d2914c76d058c05a28ddbe77403
David Heyman <dave@axismaps.com>
2019-05-20 23:02:15 +0100

    Another bad option

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const db = require('./db');
 
-const offset = 10000;
+let tables = [];
+const offset = 1000;
 const geoms = {
   point: 'MULTIPOINT',
   line: 'MULTILINESTRING',
   poly: 'MULTIPOLYGON',
 };
+
+function loadTable(t) {
+  console.log(`LOADING ${t}`);
+  const table = t.replace(/_evw$/gm, '');
+  return db.local.any(`SELECT tablename FROM pg_catalog.pg_tables WHERE tablename = '${table}'`)
+    .then((tableExists) => {
+      let q = 'SELECT \'Hello World!\'';
+      if (tableExists.length === 0) {
+        const geom = table.match(/(point|line|poly)/gmi);
+        console.log(`CREATING ${table}`);
+        q = `CREATE TABLE ${table} (
+              "objectid" int,
+              "name" text,
+              "firstyear" int,
+              "lastyear" int,
+              "firstdate" int,
+              "lastdate" int,
+              "type" text,
+              "geom" geometry(${geoms[geom]}, 4326),
+              PRIMARY KEY ("objectid")
+          );`;
+      }
+      return db.local.any(q).then(() => loadRemote(table, 0)).catch((e) => {
+        console.log(e);
+        return loadTable(tables.shift());
+      });
+    }).catch((e) => {
+      console.log(e);
+    });
+}
 
 function loadRemote(table, start) {
   return db.remote.many(`SELECT
@@ -18,7 +49,7 @@ function loadRemote(table, start) {
       type,
       ST_AsText(ST_Transform(shape, 4326)) AS geom
     FROM uilvim.${table}_evw
-    LIMIT ${start + offset} OFFSET ${start}`).then((geo) => {
+    LIMIT ${offset} OFFSET ${start}`).then((geo) => {
     console.log(`LOADED ${table} FROM ${start}`);
     const features = geo.filter(g => g.geom !== 'EMPTY');
     return db.local.tx((local) => {
@@ -39,9 +70,10 @@ function loadRemote(table, start) {
       console.log(`UPDATING ${table}`);
       return local.batch(updates).then(() => {
         if (geo.length === offset) {
+          console.log(`LOADING ${offset} MORE ROWS FROM ${table}`);
           return loadRemote(table, start + offset);
         }
-        return null;
+        return loadTable(tables.shift());
       });
     });
   });
@@ -49,36 +81,8 @@ function loadRemote(table, start) {
 
 db.remote.many("SELECT viewname FROM pg_catalog.pg_views WHERE viewname LIKE '%evw'")
   .then((views) => {
-    const tables = views.map(v => v.viewname);
-    const loader = tables.map((t) => {
-      console.log(`LOADING ${t}`);
-      const table = t.replace(/_evw$/gm, '');
-      return db.local.any(`SELECT tablename FROM pg_catalog.pg_tables WHERE tablename = '${table}'`)
-        .then((tableExists) => {
-          let q = 'SELECT \'Hello World!\'';
-          if (tableExists.length === 0) {
-            const geom = table.match(/(point|line|poly)/gmi);
-            console.log(`CREATING ${table}`);
-            q = `CREATE TABLE ${table} (
-                  "objectid" int,
-                  "name" text,
-                  "firstyear" int,
-                  "lastyear" int,
-                  "firstdate" int,
-                  "lastdate" int,
-                  "type" text,
-                  "geom" geometry(${geoms[geom]}, 4326),
-                  PRIMARY KEY ("objectid")
-              );`;
-          }
-          return db.local.any(q).then(() => loadRemote(table, 0)).catch((e) => {
-            console.log(e);
-          });
-        }).catch((e) => {
-          console.log(e);
-        });
-    });
-    Promise.resolve(loader);
+    tables = views.map(v => v.viewname);
+    loadTable(tables.shift());
   }).catch((e) => {
     console.log(e);
   });
